6d806c3ed21de2cbd6bf8cdd3c1e37ee24b139ab
crimeacs <crimeacs@yandex.ru>
2021-08-24 13:41:30 +0200

    Figuring DM structure

--- sedenoss/data.py
+++ sedenoss/data.py
@@ -5,11 +5,13 @@ from torch.utils.data import Dataset, DataLoader
 import pytorch_lightning as pl
 
 import random
-
+from sklearn.model_selection import train_test_split
+from torch_audiomentations import Compose, Gain, HighPassFilter, LowPassFilter, PolarityInversion, PeakNormalization
+import xarray
 
 # @title Define dataset class
 class TrainSignals(Dataset):
-    def __init__(self, data, noise, transform_signal=False, transform_noise=False, denoising_mode=False, test=False):
+    def __init__(self, data, noise, transform_signal, transform_noise, denoising_mode=False, test=False):
         self.data = data
         self.noise = noise
         self.transform_signal = transform_signal
@@ -68,24 +70,122 @@ class TrainSignals(Dataset):
         return len(self.data)
 
 class DataModule(pl.LightningDataModule):
-
-    def __init__(self, batch_size: int = 192, num_workers: int = 4):
+    def __init__(self, batch_size: int = 192, num_workers: int = 4, denoising_mode: bool = False,
+                 data_path: str = '/gdrive/MyDrive/Seismic GAN/STEAD_data_JUL_2021/waveforms_signal.nc',
+                 noise_path: str = '/gdrive/MyDrive/Seismic GAN/STEAD_data_JUL_2021/waveforms_noise.nc',
+                 ):
         super().__init__()
         self.batch_size = batch_size
         self.num_workers = num_workers
+        self.denoising_mode = denoising_mode
+        self.data_path = data_path
+        self.noise_path = noise_path
+
+    def worker_init_fn(self, worker_id):
+        np.random.seed(np.random.get_state()[1][0] + worker_id)
+
+    def choose_agmentations(self):
+        # Define augmentations
+        # Initialize augmentation callable
+        if self.denoising_mode:
+            augmentation_signal = Compose(
+                transforms=[
+                    HighPassFilter(
+                        min_cutoff_freq=0.5,
+                        max_cutoff_freq=1.5,
+                        mode="per_example",
+                        p=1,
+                    ),
+
+                    LowPassFilter(
+                        min_cutoff_freq=10,
+                        max_cutoff_freq=14,
+                        mode="per_example",
+                        p=0.5,
+                    ),
+
+                    PolarityInversion(p=0.5)
+                ]
+            )
+
+            augmentation_noise = Compose(
+                transforms=[
+                    Gain(
+                        min_gain_in_db=-10.0,
+                        max_gain_in_db=0,
+                        p=1,
+                    ),
+
+                    HighPassFilter(
+                        min_cutoff_freq=0.1,
+                        max_cutoff_freq=1,
+                        mode="per_example",
+                        p=0.5,
+                    ),
+
+                    LowPassFilter(
+                        min_cutoff_freq=10,
+                        max_cutoff_freq=14,
+                        mode="per_example",
+                        p=0.5,
+                    ),
+
+                    PolarityInversion(p=0.5)
+                ]
+            )
+        else:
+            augmentation_noise = Compose(
+                transforms=[
+
+                    HighPassFilter(
+                        min_cutoff_freq=0.5,
+                        max_cutoff_freq=1.5,
+                        mode="per_channel",
+                        p=0.5,
+                    ),
+
+                    LowPassFilter(
+                        min_cutoff_freq=5,
+                        max_cutoff_freq=14,
+                        mode="per_channel",
+                        p=0.5,
+                    ),
+
+                    PolarityInversion(p=0.5),
+
+                    PeakNormalization(
+                        mode="per_example",
+                        p=1
+                    ),
+
+                ]
+            )
+            augmentation_signal = augmentation_noise
+
+        return augmentation_signal, augmentation_noise
+
+    def produce_datasets(self):
+        da_data = xarray.open_dataset(self.data_path, engine='netcdf4')
+        da_noise = xarray.open_dataset(self.noise_path, engine='netcdf4')
+        train_val_data = da_data.sel(channel=0).to_array().values[0]
+        train_val_noise = da_noise.sel(channel=0).to_array().values[0]
+
+        data_train, data_val = train_test_split(train_val_data, train_size=0.95)
+        noise_train, noise_val = train_test_split(train_val_noise, train_size=0.95)
+
+        augmentation_signal, augmentation_noise = self.choose_agmentations()
+
+        train_dataset = TrainSignals(data_train, noise_train, transform_signal=augmentation_signal,
+                                          transform_noise=augmentation_noise, denoising_mode=self.denoising_mode)
+        val_dataset = TrainSignals(data_val, noise_val, transform_signal=False, transform_noise=False,
+                                        denoising_mode=self.denoising_mode)
+
+        return train_dataset, val_dataset
 
     def train_dataloader(self):
         return DataLoader(train_dataset, batch_size=self.batch_size, num_workers=self.num_workers,
-                          shuffle=True, pin_memory=True, worker_init_fn=worker_init_fn)
+                          shuffle=True, pin_memory=True, worker_init_fn=self.worker_init_fn)
 
     def val_dataloader(self):
         return DataLoader(val_dataset, batch_size=self.batch_size, num_workers=self.num_workers,
-                          shuffle=False, pin_memory=True, worker_init_fn=worker_init_fn)
-
-    def test_dataloader(self):
-        return DataLoader(test_dataset, batch_size=self.batch_size, num_workers=self.num_workers,
-                          shuffle=False, pin_memory=True, worker_init_fn=worker_init_fn)
-
-def worker_init_fn(worker_id):
-    np.random.seed(np.random.get_state()[1][0] + worker_id)
-
+                          shuffle=False, pin_memory=True, worker_init_fn=self.worker_init_fn)
--- sedenoss/train.py
+++ sedenoss/train.py
@@ -1,132 +1,36 @@
-import xarray
 
-from sklearn.model_selection import train_test_split
-from torch_audiomentations import Compose, Gain, HighPassFilter, LowPassFilter, PolarityInversion, PeakNormalization
+import argparse, yaml
+
 
 from pytorch_lightning.callbacks.model_checkpoint import ModelCheckpoint
-
-def choose_agmentations(denoising_mode):
-    # Define augmentations
-    # Initialize augmentation callable
-    if denoising_mode:
-        augmentation_signal = Compose(
-            transforms=[
-                HighPassFilter(
-                    min_cutoff_freq=0.5,
-                    max_cutoff_freq=1.5,
-                    mode="per_example",
-                    p=1,
-                ),
-
-                LowPassFilter(
-                    min_cutoff_freq=10,
-                    max_cutoff_freq=14,
-                    mode="per_example",
-                    p=0.5,
-                ),
-
-                PolarityInversion(p=0.5)
-            ]
-        )
-
-        augmentation_noise = Compose(
-            transforms=[
-                Gain(
-                    min_gain_in_db=-10.0,
-                    max_gain_in_db=0,
-                    p=1,
-                ),
-
-                HighPassFilter(
-                    min_cutoff_freq=0.1,
-                    max_cutoff_freq=1,
-                    mode="per_example",
-                    p=0.5,
-                ),
-
-                LowPassFilter(
-                    min_cutoff_freq=10,
-                    max_cutoff_freq=14,
-                    mode="per_example",
-                    p=0.5,
-                ),
-
-                PolarityInversion(p=0.5)
-            ]
-        )
-    else:
-        augmentation_noise = Compose(
-            transforms=[
-
-                HighPassFilter(
-                    min_cutoff_freq=0.5,
-                    max_cutoff_freq=1.5,
-                    mode="per_channel",
-                    p=0.5,
-                ),
-
-                LowPassFilter(
-                    min_cutoff_freq=5,
-                    max_cutoff_freq=14,
-                    mode="per_channel",
-                    p=0.5,
-                ),
-
-                PolarityInversion(p=0.5),
-
-                PeakNormalization(
-                    mode="per_example",
-                    p=1
-                ),
-
-            ]
-        )
-        augmentation_signal = augmentation_noise
-
-    return augmentation_signal, augmentation_noise
-
-def produce_datasets(data_path='/gdrive/MyDrive/Seismic GAN/STEAD_data_JUL_2021/waveforms_signal.nc',
-                     noise_path='/gdrive/MyDrive/Seismic GAN/STEAD_data_JUL_2021/waveforms_noise.nc',
-                     denoising_mode=True):
-
-    da_data = xarray.open_dataset(data_path,
-                                  engine='netcdf4')
-    da_noise = xarray.open_dataset(noise_path,
-                                   engine='netcdf4')
-    train_val_data = da_data.sel(channel=0).to_array().values[0]
-    train_val_noise = da_noise.sel(channel=0).to_array().values[0]
-
-    if 'data_train' not in globals():
-        data_train, data_val = train_test_split(train_val_data, train_size=0.95)
-        noise_train, noise_val = train_test_split(train_val_noise, train_size=0.95)
-
-    augmentation_signal, augmentation_noise = choose_agmentations(denoising_mode)
-
-    train_dataset = TrainSignals(data_train, noise_train, transform_signal=augmentation_signal, transform_noise=augmentation_noise, denoising_mode=denoising_mode)
-    val_dataset   = TrainSignals(data_val, noise_val, transform_signal=False, transform_noise=False, denoising_mode=denoising_mode)
-
-    return train_dataset, val_dataset
+from data import DataModule
 
 # generate_data_samples(test_dataset)
 
-mc = ModelCheckpoint(monitor='val_loss', save_top_k=3)
+if __name__ == "__main__":
+    data_path = '/gdrive/MyDrive/Seismic GAN/STEAD_data_JUL_2021/waveforms_signal.nc'
+    noise_path = '/gdrive/MyDrive/Seismic GAN/STEAD_data_JUL_2021/waveforms_noise.nc'
 
-dm = DataModule()
-model = FaSNet_base()#.load_from_checkpoint(checkpoint_path="/gdrive/MyDrive/TraML/lightning_logs/version_29/checkpoints/epoch=4-step=12094.ckpt")
+    train_dataset, val_dataset = DataModule.produce_datasets(data_path, noise_path)
 
-trainer = pl.Trainer(gpus=-1,
-                     progress_bar_refresh_rate=50,
-                     default_root_dir='/gdrive/MyDrive/TraML/',
-                     benchmark=True,
+    mc = ModelCheckpoint(monitor='val_loss', save_top_k=3)
 
-                     max_epochs=200,
-                     terminate_on_nan=True,
+    dm = DataModule()
+    model = FaSNet_base()#.load_from_checkpoint(checkpoint_path="/gdrive/MyDrive/TraML/lightning_logs/version_29/checkpoints/epoch=4-step=12094.ckpt")
 
-                     callbacks=[mc],
-                     num_sanity_val_steps=1,
-                     precision=16,
+    trainer = pl.Trainer(gpus=-1,
+                         progress_bar_refresh_rate=50,
+                         default_root_dir='/gdrive/MyDrive/TraML/',
+                         benchmark=True,
 
-                    #  fast_dev_run = True,
-                     )
+                         max_epochs=200,
+                         terminate_on_nan=True,
 
-trainer.fit(model, dm)
+                         callbacks=[mc],
+                         num_sanity_val_steps=1,
+                         precision=16,
+
+                        #  fast_dev_run = True,
+                         )
+
+    trainer.fit(model, dm)
